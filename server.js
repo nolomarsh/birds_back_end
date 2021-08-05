95879d5ec0e97b168488f73bc000112db555eba4
Nolo Marsh <nolo.marsh@gmail.com>
2021-08-05 15:25:11 -0400

    set up basics of server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+//****************
+//* Dependencies *
+//****************
+const cors = require('cors')
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+
+//***************
+//* Middleware **
+//***************
+app.use(express.json())
+app.use(cors())
+
+//***************
+//* Controllers *
+//***************
+
+
+//***************
+//*** Routes ****
+//***************
+app.get('/', (req,res) => {
+    res.redirect('/birds')
+})
+
+//***************
+//** Listeners **
+//***************
+app.listen(3000, () => {
+    console.log('listening')
+})
+
+mongoose.connect('mongodb://localhost:27017/merncrud', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongod...')
+})
